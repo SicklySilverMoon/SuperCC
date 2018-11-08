357695611d3e2047d0616c78199962b74a899bee
jgdhs27 <markusobendrauf@gmail.com>
2018-11-08 13:30:02 +0100

    Moved compressor here. Added getFirstState()

--- src/emulator/SavestateManager.java
+++ src/emulator/SavestateManager.java
@@ -6,9 +6,13 @@ import game.SaveState;
 import util.ByteList;
 import util.TreeNode;
 
-import java.security.KeyException;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Stack;
+
+import static game.SaveState.COMPRESSED;
+import static game.SaveState.RLE_END;
+import static game.SaveState.RLE_MULTIPLE;
 
 public class SavestateManager {
     
@@ -49,6 +53,12 @@ public class SavestateManager {
         return currentNode.getData();
     }
     
+    public byte[] getStartingState() {
+        TreeNode<byte[]> state = currentNode;
+        while (state.hasParent()) state = state.getParent();
+        return state.getData();
+    }
+    
     public ByteList getMoves(){
         return currentMoves;
     }
@@ -73,5 +83,100 @@ public class SavestateManager {
         }
         return history;
     }
-
+    
+    private class SavestateCompressor implements Runnable{
+        
+        private static final int LAYER_BG_LOCATION = 3,
+            LAYER_FG_LOCATION = LAYER_BG_LOCATION + 32 * 32,
+            LAYER_FG_END = LAYER_FG_LOCATION + 32 * 32;
+        
+        private final Stack<TreeNode<byte[]>> uncompressedSavestates;
+        private final ByteList list;
+        
+        private final Thread thread;
+        
+        void add(TreeNode<byte[]> n){
+            uncompressedSavestates.add(n);
+            synchronized(thread) {
+                thread.notify();
+            }
+        }
+        
+        @Override
+        public void run(){
+            while (true) {
+                try {
+                    if (uncompressedSavestates.isEmpty()) {
+                        synchronized (thread) {
+                            thread.wait();
+                        }
+                    }
+                    else {
+                        compress(uncompressedSavestates.pop());
+                    }
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        private void rleCompress(byte[] uncompressed, ByteList out, int startIndex, int length){
+            int lastOrdinal = uncompressed[startIndex];
+            int ordinal;
+            int copyCount = -1;
+            for (int i = startIndex; i < startIndex + length; i++) {
+                ordinal = uncompressed[i];
+                if (ordinal == lastOrdinal){
+                    if (copyCount == 255){
+                        out.add(RLE_MULTIPLE);
+                        out.add(copyCount);
+                        copyCount = 0;
+                        out.add(ordinal);
+                    }
+                    else copyCount++;
+                }
+                else {
+                    if (copyCount != 0){
+                        out.add(RLE_MULTIPLE);
+                        out.add(copyCount);
+                    }
+                    out.add(lastOrdinal);
+                    copyCount = 0;
+                    lastOrdinal = ordinal;
+                }
+            }
+            if (copyCount != 0){
+                out.add(RLE_MULTIPLE);
+                out.add(copyCount);
+            }
+            out.add(lastOrdinal);
+            out.add(RLE_END);
+        }
+        
+        private void compress(TreeNode<byte[]> n){
+            list.clear();
+            byte[] uncompressedState = n.getData();
+            rleCompress(uncompressedState, list, LAYER_BG_LOCATION, 32*32);
+            rleCompress(uncompressedState, list, LAYER_FG_LOCATION, 32*32);
+            byte[] out = new byte[uncompressedState.length - 2 * 32 * 32 + list.size()];
+            out[0] = COMPRESSED;
+            out[1] = uncompressedState[1];
+            out[2] = uncompressedState[2];
+            list.copy(out, 3);
+            System.arraycopy(uncompressedState, LAYER_FG_END, out, 3 + list.size(), uncompressedState.length - LAYER_FG_END);
+            n.setData(out);
+        }
+        
+        SavestateCompressor(){
+            uncompressedSavestates = new Stack<>();
+            list = new ByteList();
+            thread = new Thread(() -> run());
+            thread.setPriority(Thread.MIN_PRIORITY);
+            thread.start();
+        }
+        
+    }
+    
+    
 }
