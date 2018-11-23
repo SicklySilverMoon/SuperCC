03f7fa4626eb40b13322991891d0020dc96c6825
jgdhs27 <markusobendrauf@gmail.com>
2018-11-23 17:29:15 +0100

    Added useful methods to BrownButton

--- src/game/button/BrownButton.java
+++ src/game/button/BrownButton.java
@@ -6,14 +6,28 @@ import game.Position;
 public class BrownButton extends Button {
     
     private final int trapIndex;
+    private final Position trapPosition;
     
     @Override
     public void press(Level level) {
         level.getOpenTraps().set(trapIndex);
     }
     
-    public BrownButton(Position buttonPosition, int trapIndex) {
+    public void release(Level level) {
+        level.getOpenTraps().set(trapIndex, false);
+    }
+    
+    public int getTrapIndex() {
+        return trapIndex;
+    }
+    
+    public Position getTrapPosition() {
+        return trapPosition;
+    }
+    
+    public BrownButton(Position buttonPosition, Position trapPosition, int trapIndex) {
         super(buttonPosition);
+        this.trapPosition = trapPosition;
         this.trapIndex = trapIndex;
     }
     
