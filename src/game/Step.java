44bd57f3a4e18029abddf2675146ac98a335bb21
jgdhs27 <markusobendrauf@gmail.com>
2018-12-02 10:23:30 +0100

    added methods for tws conversion

--- src/game/Step.java
+++ src/game/Step.java
@@ -3,4 +3,15 @@ package game;
 public enum Step{
     ODD,
     EVEN;
+    
+    public static Step fromTWS(int n) {
+        if (n >>> 5 == 1) return Step.ODD;
+        else return Step.EVEN;
+    }
+    
+    public byte toTWS() {
+        if (this == EVEN) return 0;
+        else return 0b100000;
+    }
+    
 }
